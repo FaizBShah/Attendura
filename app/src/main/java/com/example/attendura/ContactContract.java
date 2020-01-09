fdd72d424ee3552c67d8dbb12fd20d8b3e3241d6
Faiz Bachoo Shah <54135532+FaizBShah@users.noreply.github.com>
2020-01-09 15:05:37 +0530

    Minor changes

--- app/src/main/java/com/example/attendura/ContactContract.java
+++ app/src/main/java/com/example/attendura/ContactContract.java
@@ -2,11 +2,13 @@ package com.example.attendura;
 
 public class ContactContract
 {
-    private ContactContract {} {}
+    private ContactContract()
+    {
+    }
 
     public static class ContactEntry
     {
-        public static final String ATTENDANCE_NAME = "contact_info";
+        public static final String TABLE_NAME = "contact_info";
         public static final String CONTACT_DATE = "date";
     }
 }
